962f3cce0b1bef9ce5e237a450bc58f2b4c900cd
Sambhav-Saraswat123 <76476750+Sambhav-Saraswat123@users.noreply.github.com>
2021-03-09 10:55:15 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // JS
+const $ = jQuery;
+swal;
+$('#ss').click(() => {
+    swal({
+        title: "Sambhav Saraswat", 
+        text: "Sambhav Saraswat official website re-designed",
+        icon: "info",
+        button: "OK"
+    });
+});
